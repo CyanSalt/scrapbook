02bdd7f2f5e500ed9afe1dfe4165d2cdbff1142a
Sun Xiaoran <c.salt@hotmail.com>
2022-03-16 11:31:03 +0800

    fix: theme style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import sairinConfig from '../sairin.config'
 // import theme style
 import "@sairinjs/sairin-theme-minimal/style.css";
 
+import './custom.css'
+
 export const sairin = new Sairin(sairinConfig)
 
 function MyApp({ Component, pageProps }) {
